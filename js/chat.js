2fdcd340188e8ebb84e90ab1e44c19bef15a551d
biancaroman <biancaroman1425@gmail.com>
2023-06-07 18:58:13 -0300

    link para o chat-gpt

---
+++ js/chat.js
@@ -0,0 +1,4 @@
+var mensagemInput = document.getElementById('mensagemInput');
+mensagemInput.addEventListener('click',function(){
+    window.location.href = 'https://chat.openai.com/auth/login';
+});
